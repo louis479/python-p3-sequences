2d99088e8bef3612a7e78e8d79591a8f311e7a35
louis-ngugi <louisngugi858@gmail.com>
2025-02-17 16:25:55 +0300

    updated the range of fibonacci sequences in lib/ sequences.py

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
+# fibonacci_list = [0,1,1,2,3,5,8,13,21]
+# month_tuple = ('January', 'February', 'March', 'April', 'May', 'June')
+# even_numbers = range(0, 49, 98)
+# sentence_string = "Python is awesome!"
+# def print_fibonacci(length):
+#     pass
+
+
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+    for _ in range(2, length):
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    print(fibonacci_sequence[:length])
+
+# Example usage:
+print_fibonacci(10) 
